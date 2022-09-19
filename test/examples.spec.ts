cbb400989c54d50e30e790487bbdb9be7d1841e2
André Teixeira <andremarcondesteixeira@gmail.com>
2022-09-19 14:21:29 -0300

    wip

--- test/examples.spec.ts
+++ test/examples.spec.ts
@@ -2,12 +2,20 @@ import { Asset, Liability, sum } from "../src/asset_liability";
 import money from "../src/money";
 
 test("Example 1", () => {
+    // John decided to open a company.
+    // He invested $20,000 to buy raw materials.
+    // In the first month of operation, he used $5,000 worth of raw materials and had
+    // $25,000 of total sales.
+    // He also has $10,000 of bills to pay.
+
     const assets: Asset[] = [
         new Asset("Raw materials", money("USD", 15_000_00)),
         new Asset("Total sales", money("USD", 25_000_00)),
     ];
 
-    const liabilities: Liability[] = [new Liability("Bills to pay", money("USD", 10_000_00))];
+    const liabilities: Liability[] = [
+        new Liability("Bills to pay", money("USD", 10_000_00)),
+    ];
 
     // The equity is a liability because the company must return it to the shareholders
     const equity: Liability[] = [
@@ -28,19 +36,28 @@ test("Example 1", () => {
 });
 
 test("Example 2", () => {
+    // You own a computer that is worth $5,000 if you sell it today.
+    // You bought it for $7,500 and paid $4,000 so far.
+    // Apart from that, you have $2,000 in your checkings account and $1,500 of bills to be paid.
+
     const assets: Asset[] = [
         new Asset("Computer", money("USD", 5_000_00)),
         new Asset("Money in the checkings account", money("USD", 2_000_00)),
     ];
 
     const liabilities: Liability[] = [
-        new Liability("Installments payable for the computer", money("USD", 3_500_00)),
+        new Liability(
+            "Installments payable for the computer",
+            money("USD", 3_500_00)
+        ),
         new Liability("Bills to be paid", money("USD", 1_500_00)),
     ];
 
     // In the case of a person, the equity is how much money you actually have
     // It is the value of your assets subtracted by all your present and future expenses
-    const equity: Liability[] = [new Liability("What you actually have", money("USD", 2_000_00))];
+    const equity: Liability[] = [
+        new Liability("What you actually have", money("USD", 2_000_00)),
+    ];
 
     const leftSideOfTheEquation = sum(assets);
     const rightSideOfTheEquation = sum(liabilities).add(sum(equity));
